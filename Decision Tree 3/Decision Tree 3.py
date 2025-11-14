2dc5ae89de1dc3fd1d9fd18f338d710a2dabbbc8
Shreyan S <192471018.simats@saveetha.com>
2025-11-14 14:21:40 +0530

    Update Decision Tree 3.py

--- Decision Tree 3/Decision Tree 3.py
+++ Decision Tree 3/Decision Tree 3.py
@@ -1,25 +1,40 @@
+import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier, export_text
 
-outlook=['Sunny','Sunny','Overcast','Rainy','Rainy','Rainy','Overcast','Sunny','Sunny','Rainy','Sunny','Overcast','Overcast','Rainy']
-temperature=['Hot','Hot','Hot','Mild','Cool','Cool','Cool','Mild','Cool','Mild','Mild','Mild','Hot','Mild']
-humidity=['High','High','High','High','Normal','Normal','High','High','Normal','Normal','Normal','High','Normal','High']
-windy=['False','True','False','False','False','True','True','False','False','False','True','True','False','True']
-play=['No','No','Yes','Yes','Yes','No','Yes','No','Yes','Yes','Yes','Yes','Yes','No']
+# Data setup
+data = {
+    'Outlook': ['Sunny', 'Sunny', 'Overcast', 'Rainy', 'Rainy', 'Rainy', 'Overcast', 'Sunny', 'Sunny', 'Rainy', 'Sunny', 'Overcast', 'Overcast', 'Rainy'],
+    'Temperature': ['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild', 'Cool', 'Mild', 'Mild', 'Mild', 'Hot', 'Mild'],
+    'Humidity': ['High', 'High', 'High', 'High', 'Normal', 'Normal', 'High', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'High'],
+    'Windy': ['False', 'True', 'False', 'False', 'False', 'True', 'True', 'False', 'False', 'False', 'True', 'True', 'False', 'True'],
+    'Play': ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
+}
 
-le1=LabelEncoder()
-le2=LabelEncoder()
-le3=LabelEncoder()
-le4=LabelEncoder()
-le5=LabelEncoder()
+# Create DataFrame
+df = pd.DataFrame(data)
 
-o=le1.fit_transform(outlook)
-t=le2.fit_transform(temperature)
-h=le3.fit_transform(humidity)
-w=le4.fit_transform(windy)
-p=le5.fit_transform(play)
+# Encode categorical features
+le_outlook = LabelEncoder().fit_transform(df['Outlook'])
+le_temp = LabelEncoder().fit_transform(df['Temperature'])
+le_humidity = LabelEncoder().fit_transform(df['Humidity'])
+le_windy = LabelEncoder().fit_transform(df['Windy'])
+le_play = LabelEncoder().fit_transform(df['Play'])
 
-X=[[o[i],t[i],h[i],w[i]] for i in range(len(o))]
+# Prepare features and target
+X = pd.DataFrame({
+    'Outlook': le_outlook,
+    'Temperature': le_temp,
+    'Humidity': le_humidity,
+    'Windy': le_windy
+})
 
-clf=DecisionTreeClassifier().fit(X,p)
-print(export_text(clf,feature_names=['Outlook','Temperature','Humidity','Windy']))
+y = le_play
+
+# Fit decision tree classifier
+clf = DecisionTreeClassifier()
+clf.fit(X, y)
+
+# Export decision tree in text format
+tree_text = export_text(clf, feature_names=['Outlook', 'Temperature', 'Humidity', 'Windy'])
+print(tree_text)
