ef65f7f97ca5afc3e09256750d396c45adcf153a
Shreyan S <192471018.simats@saveetha.com>
2025-11-14 14:20:59 +0530

    Update Decision Tree 2.py

--- Decision Tree 2/Decision Tree 2.py
+++ Decision Tree 2/Decision Tree 2.py
@@ -1,10 +1,26 @@
+import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, export_text
 
-a1=['T','T','T','F','F','F']
-a2=['T','T','F','F','T','T']
-y=['+','+','-','+','-','-']
+# Prepare data using pandas DataFrame
+data = {
+    'a1': ['T', 'T', 'T', 'F', 'F', 'F'],
+    'a2': ['T', 'T', 'F', 'F', 'T', 'T'],
+    'label': ['+', '+', '-', '+', '-', '-']
+}
 
-X=[[1 if a1[i]=='T' else 0,1 if a2[i]=='T' else 0] for i in range(len(a1))]
-clf=DecisionTreeClassifier().fit(X,y)
-tree=export_text(clf,feature_names=['a1','a2'])
-print(tree)
+df = pd.DataFrame(data)
+
+# Convert features to binary numeric form
+df['a1_num'] = df['a1'].apply(lambda x: 1 if x == 'T' else 0)
+df['a2_num'] = df['a2'].apply(lambda x: 1 if x == 'T' else 0)
+
+X = df[['a1_num', 'a2_num']]
+y = df['label']
+
+# Train Decision Tree Classifier
+clf = DecisionTreeClassifier()
+clf.fit(X, y)
+
+# Export and print tree in text format
+tree_text = export_text(clf, feature_names=['a1', 'a2'])
+print(tree_text)
